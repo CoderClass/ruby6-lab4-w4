c4e7c3aa1a8d7ccadbd8ea9a5d7e1ece06b4b439
phuonghoang713 <phuonghoang713@gmail.com>
2017-07-05 20:17:26 +0700

    milestone 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,17 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      # respond_to :js
+      redirect_to room_messages_path(@room)
+    end
+  end
+  private
+  def message_params
+    params.require(:message).permit(:username, :content)
+  end
 end
